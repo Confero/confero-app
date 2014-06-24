9007bc339251c8d10e6797496ce944aac03c9de6
rylan <rylan@icottrell.com>
2014-06-24 01:41:52 +0000

    Sessions list

---
+++ www/js/services/ConferenceService.js
@@ -0,0 +1,12 @@
+angular.module('confero.conferenceService', ['ngResource'])
+
+.factory('Sessions', ['$resource', '$cacheFactory',
+	function($resource, $cacheFactory) {
+		var www = 'http://'+ location.hostname + ':3000';
+		var res = www + '/conference/:id/sessions';
+		return $resource( res, {}, {
+			'get' : {method:'GET', params:{id:''}, cache: $cacheFactory}
+		});
+	}
+]);
+
